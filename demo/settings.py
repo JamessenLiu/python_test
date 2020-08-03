14f0369922ef87c6ab918a83cca8a20f4affeec1
liumaosen <461614816@qq.com>
2020-08-03 16:06:44 +0800

    initial

--- demo/settings.py
+++ demo/settings.py
@@ -78,16 +78,16 @@ WSGI_APPLICATION = 'demo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'rpc',
-        'USER': 'postgres',
-        'PASSWORD': 'jamessen',
-        'HOST': 'localhost',
-        'PORT': 5432
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'rpc',
+#         'USER': 'postgres',
+#         'PASSWORD': 'jamessen',
+#         'HOST': 'localhost',
+#         'PORT': 5432
+#     }
+# }
 
 
 # Password validation
