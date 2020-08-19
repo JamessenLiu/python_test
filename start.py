1250063178f957ba158199dd61d0e7d452457f75
liumaosen <461614816@qq.com>
2020-08-19 21:28:21 +0800

    add docker mongo

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
 import os
 
 os.system("uwsgi -w demo.wsgi -s :8000 --processes=4 &")
-# os.system("daphne -b 0.0.0.0 -p 8001 --proxy-headers demo.asgi:application &")
+os.system("daphne -b 0.0.0.0 -p 8001 --proxy-headers demo.asgi:application > logs/asgi.log 2>&1 &")
