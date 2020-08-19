cc0026d0a5994f23eb5dfaf4d2625d3adba8799e
liumaosen <461614816@qq.com>
2020-08-19 15:12:19 +0800

    add docker mongo

--- chat/routing.py
+++ chat/routing.py
@@ -6,5 +6,5 @@ from django.conf.urls import url
 from . import consumers
 
 WEBSOCKET_URLPATTERNS = [
-    url(r'^ws/(?P<socket_id>[^/]+)/$', consumers.DeployResult),
+    url(r'^ws/(?P<socket_id>[^/]+)$', consumers.DeployResult),
 ]
